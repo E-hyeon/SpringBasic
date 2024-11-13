240f7adea4ce405cd2460c3010f38996805610bb
E-hyeon <liojung518@gmail.com>
2024-11-14 03:08:38 +0900

    sec5 - 컴포넌트 스캔과 자동 의존 관계 설정

--- hello-spring/src/main/java/helllo/hello_spring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/helllo/hello_spring/repository/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package helllo.hello_spring.repository;
 
 import helllo.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
+
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
--- hello-spring/src/main/java/helllo/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/helllo/hello_spring/service/MemberService.java
@@ -3,19 +3,24 @@ package helllo.hello_spring.service;
 import helllo.hello_spring.domain.Member;
 import helllo.hello_spring.repository.MemberRepository;
 import helllo.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
-    private  final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository){
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
     /*회원가입*/
-    public Long join(Member member){
+    public Long join(Member member) {
         //같은 이름이 있는 중복 회원x
         validateDuplicateMember(member);//중복회원 검증
         memberRepository.save(member);
@@ -24,17 +29,18 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
-                 .ifPresent(m ->{
-                        throw new IllegalStateException("이미 존재하는 회원입니다.");
-                 });
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 
     /*전체 회원 조회*/
-    public List<Member> findMembers(){
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
+
     /*특정회원 조회 메서드*/
-    public Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 }
